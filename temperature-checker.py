cfa70c7162a088ee28e939eeef25ad5cd80dabd4
Dexoor <79315365+Dexoor@users.noreply.github.com>
2023-08-21 15:10:33 +0200

    Add files via upload

--- temperature-checker.py
+++ temperature-checker.py
@@ -1,19 +1,17 @@
 # import the module
 import python_weather
-
 import asyncio
 import os
 import pandas as pd
 
 
-async def getweather():
+async def getweather(CityName):
 
   # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
   async with python_weather.Client(unit=python_weather.METRIC) as client:
     # fetch a weather forecast from a city
-    cities = pd.read_csv('data/cities.csv')
-    for cities in cities['city']:
-        weather = await client.get(cities)
+
+        weather = await client.get(CityName)
     
     # returns the current day's forecast temperature (int)
         print(cities, '-->', weather.current.temperature,'°C')   
@@ -23,5 +21,7 @@ if __name__ == '__main__':
   # for more details
   if os.name == 'nt':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-  
-  asyncio.run(getweather())
+
+cities = pd.read_csv('data/cities.csv')
+for cities in cities['city']:
+  asyncio.run(getweather(cities))
